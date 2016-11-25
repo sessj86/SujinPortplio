51a0a016118dda08fe2b1130e29fd2879d116e71
choisujin <sessj86@gmail.com>
2016-11-25 17:06:08 +0900

    dd

---
+++ include/header.php
@@ -0,0 +1,8 @@
+	<header id="portpolio_header">
+	<div class="back">
+		<h1><img src="images/icon_face.png"></h1>
+		<nav class="port_gnb">
+		<ul><li><a href="portpolio_about.html">about</a></li><li><a href="portpolio_portpolio.html">portpolio</a></li><li><a href="portpolio_experience.html">experience</a></li></ul>
+		</nav>
+	</div>
+	</header>
--- js/portpolio.js
+++ js/portpolio.js
@@ -1,3 +1,4 @@
+// 마우스오버시 썸네일 확대
 $(document).ready(function() {
 
 	//move the image in pixel
@@ -26,4 +27,21 @@ $(document).ready(function() {
 		//Hide the caption
 		$(this).find('p.caption').stop(false,true).fadeOut(200);
 	});
+});
+
+
+// gnb 고정
+$(document).ready(function(){
+
+    var nav = $('#portpolio_header');
+    var navoffset = $('#portpolio_header').offset();
+    /* offset을 이용하여 .menu(메뉴영역)의 위치값을 알아내어 navoffset에 넣어둔다 */
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= navoffset.top) {  /* 화면 스크롤 값이 메뉴영역의 top보다 값이 커지면 */
+            nav.css('position','fixed').css('top',0); /* 화면 위쪽에 고정시킨다. */
+        }else {
+            nav.css('position','absolute').css('top',0); /* 처음 메뉴영역의 top 값으로 돌리기 */
+        }
+    });
 });
